355ba139fc035b53eddd7a2ee37e4453bfcd7279
Max Leblang <max@leblang.com>
2024-07-08 15:53:28 -0500

    make delta a float

--- CNN/cnn_exp.py
+++ CNN/cnn_exp.py
@@ -143,8 +143,8 @@ def run_exp(repeats=3):
         groundtruth_path = labels_dir + 'C' + str(tag).zfill(2) + '_0725.csv'
 
         # Load in both sensor data
-        accel_input_df, groundtruth_df = load_to_df(accel_filepaths, groundtruth_path, standardized_delta=1)
-        uwb_input_df, _ = load_to_df(uwb_filepaths, groundtruth_path, standardized_delta=1)
+        accel_input_df, groundtruth_df = load_to_df(accel_filepaths, groundtruth_path, standardized_delta=1.0)
+        uwb_input_df, _ = load_to_df(uwb_filepaths, groundtruth_path, standardized_delta=1.0)
 
         # Convert uwb from cm to m
         uwb_input_df['coord_x_cm'] = uwb_input_df['coord_x_cm']/100
