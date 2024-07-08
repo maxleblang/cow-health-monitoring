70fc8b0c63ab0c3499f90b2a54f2a8371408b9ce
Max Leblang <max@leblang.com>
2024-07-08 15:28:09 -0500

    Make all sensors have 1Hz sample rate

--- CNN/config.py
+++ CNN/config.py
@@ -2,4 +2,4 @@ class Config:
     ACCELEROMETER_DATA_DIR = '../sensor_data/main_data/immu/'
     UWB_DATA_DIR = '../sensor_data/main_data/uwb/'
     BEHAVIOR_LABELS_DATA_DIR = '../sensor_data/behavior_labels/individual/'
-    NUMPY_DATA_BIN_PATH = 'numpy_binaries/data_uwb_meters.npz'
+    NUMPY_DATA_BIN_PATH = 'numpy_binaries/data_1_HZ_all_sensors.npz'
